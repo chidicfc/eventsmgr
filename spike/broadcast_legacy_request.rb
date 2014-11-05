bcd81f9f37d57821ce0c8f8f3ada61602b2d8945
chidi.uba <talk2chidicfc@yahoo.com>
2014-11-05 10:58:43 +0000

    added broadcaster and listener for legacy data

---
+++ spike/broadcast_legacy_request.rb
@@ -0,0 +1,14 @@
+require "yeasu"
+include Yeasu
+
+Yeasu::Radio.configuration do |config|
+  config.producer.name = "eventsmgr"
+end
+
+
+Radio::Tunner.broadcast tags: "ciabos,ui,inbound,legacy_data" do |transmitter|
+  transmission = Radio::Transmission.new
+
+  t = transmitter.transmit transmission
+  puts "Request sent for legacy data in transmission of id: #{t.id}"
+end
