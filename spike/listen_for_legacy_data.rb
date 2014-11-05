a377f2934b91185078d09df4823a80cdd4fa65f7
chidi.uba <talk2chidicfc@yahoo.com>
2014-11-05 10:58:43 +0000

    added broadcaster and listener for legacy data

---
+++ spike/listen_for_legacy_data.rb
@@ -0,0 +1,44 @@
+require "yeasu"
+require "pry"
+require "active_support/all"
+require 'yaml'
+
+
+include Yeasu
+#
+# Yeasu::Radio.configuration do |config|
+#   config.consumer.name = "eventsmgr"
+# end
+#
+# Radio::Tunner.listen_on "out.ciabos.ui" do |receiver|
+#
+#   receiver.receive do |transmission|
+#     binding.pry
+#
+#     puts transmission
+#
+#   end
+# end
+
+
+Radio::Tunner.listen_on "in.eventsmanager" do |receiver|
+
+  receiver.receive do |transmission|
+
+    if transmission.tags.include?("legacy_data")
+      if File.exists?("legacy_data.yml")
+        YAML.load(File.open("legacy_data.yml"))
+
+        puts "loaded from legacy_data file!"
+      else
+        File.open("legacy_data.yml", 'w') { |file| file.write(transmission.to_yaml) }
+        #File.open("ciabos.data", 'w') { |file| file.write(Oj.dump(transmission)) }
+        puts "saved legacy data to file!"
+      end
+    end
+
+
+  end
+
+
+end
