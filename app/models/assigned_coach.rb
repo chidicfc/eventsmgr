d607907a9e6c2f3a42a25d95dd8866ef85b5853d
chidi.uba <talk2chidicfc@yahoo.com>
2014-11-07 15:32:35 +0000

    assigned coaches have got emails now

--- app/models/assigned_coach.rb
+++ app/models/assigned_coach.rb
@@ -1,14 +1,16 @@
 class AssignedCoach
-    attr_accessor :name, :id, :event_id
+    attr_accessor :name, :id, :event_id, :email, :image
 
-    def initialize (id, name, event_id)
+    def initialize (id, name, email, image, event_id)
       @name = name
+      @email = email
+      @image = image
       @id = id
       @event_id = event_id
     end
 
     def self.from_hash(row)
-      assigned_coach = AssignedCoach.new row[:id], row[:name], row[:event_id]
+      assigned_coach = AssignedCoach.new row[:id], row[:name], row[:email], row[:image], row[:event_id]
       assigned_coach
     end
 
