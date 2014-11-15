a08aa583b0b8964106d75b39e8c7887558aa7e9c
chidi.uba <talk2chidicfc@yahoo.com>
2014-11-15 15:15:32 +0000

    formatted timezone

--- app/models/timezone.rb
+++ app/models/timezone.rb
@@ -1,3 +1,5 @@
+require "active_support/all"
+
 class TimeZone
 
   attr_accessor :name, :id
@@ -22,6 +24,9 @@ class TimeZone
       timezones = []
       @datastore.get_timezones do |timezone_row|
         timezone = TimeZone.from_hash(timezone_row)
+
+        time_zone = ActiveSupport::TimeZone.new timezone.name
+        timezone.name = "(GMT#{time_zone.formatted_offset}) #{time_zone.name}"
         timezones << timezone
       end
       timezones
