38833b330a7344c51ec0bb932787f78d36f76b2a
chidi.uba <talk2chidicfc@yahoo.com>
2014-12-11 13:49:10 +0000

    about to push to production; bugs with event coaches_fees and description fixed

--- web.rb
+++ web.rb
@@ -23,13 +23,13 @@ before'/' do
 end
 
 get '/' do
-  if session[:status] = true
+  if session[:status]
     @controller.load_default_coaches_fee
     @controller.display_templates
     erb :index
 
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
@@ -55,27 +55,27 @@ end
 get '/dashboard' do
 
   session.clear
-  redirect 'http://staging-eventsmgr.ciabos.com/dashboard'
+  redirect 'http://app.coachinabox.biz/dashboard'
 end
 
 get '/reset' do
-  if session[:status] = true
+  if session[:status]
     @controller = ResetTemplateController.new
     @controller.reset
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
 
 get '/event_template/:id/edit' do |n|
-  if session[:status] = true
+  if session[:status]
     @edit_template_view = EditEventTemplateView.new
     @edit_template_controller = EditEventTemplateViewController.new(@edit_template_view)
     @edit_template_controller.get n
     erb :edit_template
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
@@ -101,7 +101,7 @@ patch '/edit_template/:id' do
 end
 
 get '/new_template' do
-  if session[:status] = true
+  if session[:status]
     @new_template_view = NewEventTemplateView.new
     @new_template_controller = NewEventTemplateViewController.new
     @new_template_controller.view = @new_template_view
@@ -110,7 +110,7 @@ get '/new_template' do
     @new_template_controller.get_default_coach_fees
     erb :new_template
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 
 end
@@ -142,7 +142,7 @@ post '/new_template' do
 end
 
 delete '/event_template/:id' do
-  if session[:status] = true
+  if session[:status]
     # params.to_s
     template = DeleteEventTemplateController.new.get params[:id]
 
@@ -157,7 +157,7 @@ delete '/event_template/:id' do
     DeleteEventTemplateController.new.transmit_deleted_template template
     redirect '/'
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
@@ -175,13 +175,13 @@ patch '/event_template/:id' do
 end
 
 get '/show_archive' do
-  if session[:status] = true
+  if session[:status]
     @show_archive_view = ShowArchiveEventTemplateView.new
     @show_archive_controller = ShowArchiveEventTemplateController.new(@show_archive_view)
     @show_archive_controller.show
     erb :archive
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
@@ -194,15 +194,15 @@ patch '/archive_event_template/:id' do
 end
 
 get '/show_event_templates' do
-  if session[:status] = true
+  if session[:status]
     redirect '/'
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
 get '/:template_id/new_event' do
-  if session[:status] = true
+  if session[:status]
     @view = NewEventView.new
     @new_event_controller = NewEventViewController.new(@view)
     @new_event_controller.get params[:template_id]
@@ -217,9 +217,9 @@ get '/:template_id/new_event' do
     @view.event.start_mins = "00"
 
 
-    # @view.event.selected_time_zone = session["user_timezone"].gsub('/', ' - ') if @view.event.selected_time_zone.nil?
+    @view.event.selected_time_zone = session["user_timezone"].gsub('/', ' - ') if @view.event.selected_time_zone.nil?
 
-    @view.event.selected_time_zone = "Europe - London"
+    # @view.event.selected_time_zone = "Europe - London"
     @view.event.date = Time.now.strftime("%d/%m/%Y")
 
     if !(session["event"].nil?)
@@ -235,7 +235,7 @@ get '/:template_id/new_event' do
 
     erb :new_event
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
@@ -377,7 +377,7 @@ end
 
 get '/event/:template_id/:event_id/edit' do
 
-  if session[:status] = true
+  if session[:status]
     @view = EditEventView.new
     @edit_event_controller = EditEventViewController.new(@view)
 
@@ -398,7 +398,7 @@ get '/event/:template_id/:event_id/edit' do
 
     erb :edit_event
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
@@ -524,7 +524,7 @@ post '/event/:template_id/:event_id/edit' do
 end
 
 get '/event/:template_id/:event_id/delete' do
-  if session[:status] = true
+  if session[:status]
     @view = DeleteEventView.new
     @controller = DeleteEventController.new(@view)
 
@@ -546,18 +546,18 @@ get '/event/:template_id/:event_id/delete' do
 
     redirect '/'
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
 get '/search_templates_by_letter/:letter' do
-  if session[:status] = true
+  if session[:status]
     @view = IndexView.new
     @controller = IndexViewController.new(@view)
     @controller.display_templates_by_letter params[:letter], "active"
     erb :index
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
@@ -572,13 +572,13 @@ post '/search_templates_by_name' do
 end
 
 get '/search_archive_templates_by_letter/:letter' do
-  if session[:status] = true
+  if session[:status]
     @show_archive_view = ShowArchiveEventTemplateView.new
     @controller = ShowArchiveEventTemplateController.new(@show_archive_view)
     @controller.display_templates_by_letter params[:letter], "archive"
     erb :archive
   else
-    redirect 'http://staging-eventsmgr.ciabos.com/users/sign_in'
+    redirect 'http://app.coachinabox.biz/users/sign_in'
   end
 end
 
