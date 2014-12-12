d3e8d602ed6fa374b230e8215b16d7bd8a0a60e3
chidi.uba <talk2chidicfc@yahoo.com>
2014-12-12 14:51:21 +0000

    edited legacy data insertion file

--- lib/bands/legacy.rb
+++ lib/bands/legacy.rb
@@ -7,40 +7,46 @@ class LegacyData < Antenna::Band
     transmission.tags.include? "legacy_data"
   end
 
+  def change_duration template
+
+    unless template.duration.nil? || template.duration.empty?
+      d = template.duration.split(' ')
+    else
+      d = ["1", "hour"]
+    end
+
+    if (d[0].to_i < 10) && ((d[1].include? "hour") || (d[1].include? "min") || (d[1].include? "sec"))
+      d[0] = "0%s" % d[0]
+    end
+
+    if (d[1].include? "hour") && d[2].nil?
+      template.duration = "#{d[0]}:00"
+    elsif (d[1].include? "sec") && d[2].nil?
+      template.duration = "01:00"
+    elsif (d[1].include? "min")
+      template.duration = "00:#{d[0]}"
+    elsif (d[1].include? "hour") && (d[3].include? "min")
+      if d[2].to_i < 10
+        d[2] = "0%s" % d[2]
+      end
+      template.duration = "#{d[0]}:#{d[2]}"
+    elsif (d[1].include? "day") && d[2].nil?
+      d[0] = "#{d[0].to_i * 8}"
+      if d[0].to_i < 10
+        d[0] = "0%s" % d[0]
+      end
+      template.duration = "#{d[0]}:00"
+    end
+  end
+
   def tune
+
     puts "processing legacy data"
     begin
       transmission.event_types.each do |template|
         # StoreEventTemplate.new template
 
-        unless template.duration.nil? || template.duration.empty?
-          d = template.duration.split(' ')
-        else
-          d = ["1", "hour"]
-        end
-
-        if (d[0].to_i < 10) && ((d[1].include? "hour") || (d[1].include? "min") || (d[1].include? "sec"))
-          d[0] = "0%s" % d[0]
-        end
-
-        if (d[1].include? "hour") && d[2].nil?
-          template.duration = "#{d[0]}:00"
-        elsif (d[1].include? "sec") && d[2].nil?
-          template.duration = "01:00"
-        elsif (d[1].include? "min")
-          template.duration = "00:#{d[0]}"
-        elsif (d[1].include? "hour") && (d[3].include? "min")
-          if d[2].to_i < 10
-            d[2] = "0%s" % d[2]
-          end
-          template.duration = "#{d[0]}:#{d[2]}"
-        elsif (d[1].include? "day") && d[2].nil?
-          d[0] = "#{d[0].to_i * 8}"
-          if d[0].to_i < 10
-            d[0] = "0%s" % d[0]
-          end
-          template.duration = "#{d[0]}:00"
-        end
+        change_duration template
 
         dataset = DB[:event_templates]
         if dataset.where(:title => template.title).all.empty?
@@ -57,6 +63,7 @@ class LegacyData < Antenna::Band
 
           unless template.events.empty?
             template.events.each do |event|
+              change_duration event
               dataset = DB[:events]
               dataset.insert(:id => event.uuid,:income_amount => event.income_amount,:income_currency => event.income_currency,:title => event.subtitle,:duration => event.duration,:description => event.details,:event_template_id => template.uuid,:date => Date.parse(event.start_time).strftime("%d/%m/%Y"),:start_time => event.start_time.split(" ")[1],:timezone => ActiveSupport::TimeZone.new("#{event.timezone}").to_s,:cohort => event.cohort_name,:cohort_id => event.cohort_id)
 
