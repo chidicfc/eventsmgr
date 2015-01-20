96eb6eebc7f2984510965d0789faf1e285745d93
chidi.uba <talk2chidicfc@yahoo.com>
2015-01-20 17:09:43 +0000

    need to check transmission and check if legacy is saved right

--- lib/broadcast_legacy_request.rb
+++ lib/broadcast_legacy_request.rb
@@ -7,7 +7,8 @@ end
 
 
 
-Radio::Tunner.broadcast tags: "ciabos,ui,inbound,legacy_data,#{ENV["YEASU_ENV"]}" do |transmitter|
+#Radio::Tunner.broadcast tags: "ciabos,ui,inbound,legacy_data,#{ENV["YEASU_ENV"]}" do |transmitter|
+Radio::Tunner.broadcast tags: "ciabos,ui,inbound,legacy_data,deve" do |transmitter|
   transmission = Radio::Transmission.new
 
   t = transmitter.transmit transmission
