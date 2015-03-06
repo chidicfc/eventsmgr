1d7558a723b0c25a3c308c8dc3899d811e3fa6ca
chidi.uba <talk2chidicfc@yahoo.com>
2015-03-06 15:53:06 +0000

    checks if session[user_timezone] is nil

--- web.rb
+++ web.rb
@@ -218,8 +218,7 @@ get '/:template_id/new_event' do
 
     @view.event.start_hours = "09"
     @view.event.start_mins = "00"
-
-    timezone = session["user_timezone"].split("/")
+    timezone = session["user_timezone"].nil? ? "" : session["user_timezone"].split("/")
     #timezone = ""
     @view.event.selected_time_zone = ActiveSupport::TimeZone.new("#{timezone[1]}").to_s if @view.event.selected_time_zone.nil?
 
